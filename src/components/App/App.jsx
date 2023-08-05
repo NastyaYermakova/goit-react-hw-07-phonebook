e49be7734df3a5a1bf5de2d46c01e624d7b5120c
Nastya Yermakova <ermkvnst@icloud.com>
2023-08-05 14:00:47 +0300

    add tasks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,8 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getContacts } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { Filter } from '../Filter/Filter';
 import { ContactList } from '../ContactList/ContactList';
@@ -5,11 +10,18 @@ import { Section } from 'components/Section/Section';
 import { Container } from './App.styled';
 
 export function App() {
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(getContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <Container>
       <Section title={'Phonebook'}>
         <ContactForm />
       </Section>
+      {isLoading && !error && <b>Request in progress...</b>}
       <Section title={'Contacts'}>
         <Filter />
         <ContactList />
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,52 +1,59 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
 
 const initialState = {
-  contactsItem: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contactsItem: [],
+  isLoading: false,
+  error: null,
 };
 
-export const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState,
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        state.contactsItem.push(action.payload);
-      },
-      prepare(name, number) {
-        return {
-          payload: {
-            id: nanoid(),
-            name,
-            number,
-          },
-        };
-      },
-    },
-    deleteContact(state, action) {
-      return {
-        contactsItem: state.contactsItem.filter(
-          contact => contact.id !== action.payload
-        ),
-      };
-    },
-  },
-});
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
+const handlePending = state => {
+  state.isLoading = true;
+};
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+const handleFetchContactsFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.contactsItem = action.payload;
 };
 
-export const contactReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
+const handleAddContactsFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.contactsItem.unshift(action.payload);
+};
+
+const handleDeleteContactsFulfilled = (state, action) => {
+  console.log(action.payload.id);
+  state.isLoading = false;
+  const deleteIdx = state.contactsItem.findIndex(
+    contact => contact.id === action.payload.id
+  );
+  console.log(deleteIdx);
+  state.contactsItem.splice(deleteIdx, 1);
+};
+
+export const contactsSlice = createSlice(
+  {
+    name: 'contacts',
+    initialState,
+    extraReducers: builder =>
+      builder
+        .addCase(fetchContacts.pending, handlePending)
+        .addCase(fetchContacts.fulfilled, handleFetchContactsFulfilled)
+        .addCase(fetchContacts.rejected, handleRejected)
+        .addCase(addContact.pending, handlePending)
+        .addCase(addContact.fulfilled, handleAddContactsFulfilled)
+        .addCase(addContact.rejected, handleRejected)
+        .addCase(deleteContact.pending, handlePending)
+        .addCase(deleteContact.fulfilled, handleDeleteContactsFulfilled)
+        .addCase(deleteContact.rejected, handleRejected),
+  }
 );
 
-export const { addContact, deleteContact } = contactsSlice.actions;
+export const { fetchingInProgress, fetchingSuccess, fetchingError } =
+  contactsSlice.actions;
+
+export const contactReducer = contactsSlice.reducer;
